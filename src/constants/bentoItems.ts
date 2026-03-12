09ba8fafae85f645281420670d77cb2ceaa9309f
evrenuzuntas <evrenuzuntas@gmail.com>
2026-03-12 09:23:13 +0300

    Add new images and enhance bentoItems structure with additional entries
    - Introduced new images (famly.jpg, gpt_image.PNG, ilgaz_camp.JPG) to the public directory.
    - Updated bentoItems structure to include new entries for personal and camping experiences, improving content representation and organization.

--- src/constants/bentoItems.ts
+++ src/constants/bentoItems.ts
@@ -126,6 +126,29 @@ export const BENTO_ITEMS: BentoItemData[] = [
     link: "https://wa.me/905532028484",
     label: "Whatsapp",
   },
+  {
+    id: "Whatsapp",
+    spanX: 2,
+    icon: "text",
+    label: "I’m a Senior Front-end/Fullstack developer with 5+ years of experience and the creator of the @evoloper YouTube channel. I build modern web applications using Next.js while sharing my passion for technology, cars, and gear through creative content. Beyond the screen, I’m an avid camper and a lifelong explorer of the latest gadgets.",
+  },
+  {
+    id: "Camping",
+    spanX: 2,
+    spanY: 2,
+    image: "/ilgaz_camp.JPG",
+    label: "Ilgaz Camp",
+  },
+  {
+    id: "Family",
+    spanY: 2,
+    image: "/famly.jpg",
+    label: "My Fam",
+  },
+  {
+    id: "GPTimage",
+    image: "/gpt_image.PNG",
+  },
 ];
 
 export const YOUTUBE_ITEMS: BentoItemData[] = [
